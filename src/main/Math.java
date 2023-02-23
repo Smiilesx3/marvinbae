93e7e47b00e6d67c6d6d838432f9714431d693f1
Nils Heinemann <nils.heinemann94@web.de>
2023-02-23 09:11:29 +0100

    Project cleanup

---
+++ src/main/Math.java
@@ -0,0 +1,18 @@
+public class Math {
+    public float methodeAddition(float Zahl1, float Zahl2) {
+        return Zahl1 + Zahl2;
+    }
+
+    public float methodeSubtraction(float Zahl1, float Zahl2) {
+        return Zahl1 - Zahl2;
+    }
+
+    public float methodeMulitplication(float Zahl1, float Zahl2) {
+        return Zahl1 * Zahl2;
+    }
+
+    public float methodeDivision(float Zahl1, float Zahl2) {
+        return Zahl1 / Zahl2;
+    }
+
+}
--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,17 +1,19 @@
 public class Runner {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         float zahl1 = 13;
         float zahl2 = 3;
 
-        int addition = (int) (zahl1 + zahl2);
-        int subtraktion = (int) (zahl1 - zahl2);
-        int multiplikation = (int) (zahl1 * zahl2);
-        float division = zahl1 / zahl2;
+        Math calculator = new Math();
 
-        System.out.println("Das Ergebnis der Addition lautet: "+ addition);
-        System.out.println("Das Ergebnis der Subtraktion lautet: "+ subtraktion);
-        System.out.println("Das Ergebnis der Multiplikation lautet: "+ multiplikation);
-        System.out.println("Das Ergebnis der Division lautet: "+ division);
+        float addition = calculator.methodeAddition(zahl1, zahl2);
+        float subtraktion = calculator.methodeSubtraction(zahl1, zahl2);
+        float multiplikation = calculator.methodeMulitplication(zahl1, zahl2);
+        float division = calculator.methodeDivision(zahl1, zahl2);
+
+        System.out.println("Das Ergebnis der Addition lautet: " + addition);
+        System.out.println("Das Ergebnis der Subtraktion lautet: " + subtraktion);
+        System.out.println("Das Ergebnis der Multiplikation lautet: " + multiplikation);
+        System.out.println("Das Ergebnis der Division lautet: " + division);
     }
 }
